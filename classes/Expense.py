a7262afa38f152efb8a5acd00e4f5082df3ccb54
a7med31 <ahmed.abdulrahman3000@gmail.com>
2024-05-14 02:03:48 +0300

    classes documentation

---
+++ classes/Expense.py
@@ -0,0 +1,17 @@
+class Expense:
+    """
+    This class represents an Expense
+
+    Attributes:
+        ExpenseID(int): The ID of the Expense
+        name(string): The name of the Expense
+        amount(float): The amount of the Expense
+        category(string): The category the Expense belongs to
+        date(date): The date the Expense
+
+    """
+  def __init__(self, amount, date, category, expenseID,name):
+    self.amount = amount
+    self.date = date
+    self.category = category
+    self.expenseID = expenseID
---
+++ classes/Project.py
@@ -0,0 +1,48 @@
+class Project:
+
+    """
+    class to represent a project within a coompany
+
+    Attributes:
+        projectName (str): The name of the project.
+        projectID (int): The unique identifier of the project.
+        startDate (str): The start date of the project in YYYY-MM-DD format.
+        endDate (str): The end date of the project in YYYY-MM-DD format.
+        expenses (list): A list to store project expenses.
+    """
+    def __init__(self, projectName, projectID, startDate, endDate):
+        self.projectName = projectName
+        self.projectID = projectID
+        self.startDate = startDate
+        self.endDate = endDate
+
+    def addExpense(self, amount, date, category):
+
+         """
+        Add an expense to the project.
+
+        Args:
+            amount (float): The amount of the expense.
+            date (str): The date of the expense in YYYY-MM-DD format.
+            category (str): The category of the expense.
+        """
+        pass
+
+    def editExpense(self, expenseID, newAmount, newDate):
+
+         """
+        Edit an existing expense in the project.
+
+        Args:
+            expenseID (int): The index of the expense to edit.
+            newAmount (float): The new amount of the expense.
+            newDate (str): The new date of the expense in YYYY-MM-DD format.
+        """
+        pass
+
+    def viewProjectExpenses(self):
+        """
+        View all expenses associated with the project.
+        """
+        pass
+
